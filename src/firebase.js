ca084dadb6cae1af0c276733a21c8850525f0ab6
pudgefury <bahubali0928@mail.ru>
2021-08-30 09:47:29 -0700

    initial

---
+++ src/firebase.js
@@ -0,0 +1,88 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBNwehlYXW_2oJvkPMiaTMxpcsxWkYXyO8",
+    authDomain: "fir-auth-ebe9a.firebaseapp.com",
+    projectId: "fir-auth-ebe9a",
+    storageBucket: "fir-auth-ebe9a.appspot.com",
+    messagingSenderId: "403715922304",
+    appId: "1:403715922304:web:4b1e59caa84fab63908965",
+    measurementId: "G-F8P7L7BHQ8"
+  };
+
+const app = firebase.initializeApp(firebaseConfig);
+const auth = app.auth();
+const db = app.firestore();
+
+const googleProvider = new firebase.auth.GoogleAuthProvider();
+
+const signInWithGoogle = async () => {
+  try {
+    const res = await auth.signInWithPopup(googleProvider);
+    const user = res.user;
+    const query = await db
+      .collection("users")
+      .where("uid", "==", user.uid)
+      .get();
+    if (query.docs.length === 0) {
+      await db.collection("users").add({
+        uid: user.uid,
+        name: user.displayName,
+        authProvider: "google",
+        email: user.email,
+      });
+    }
+  } catch (err) {
+    console.error(err);
+    alert(err.message);
+  }
+};
+
+const signInWithEmailAndPassword = async (email, password) => {
+  try {
+    await auth.signInWithEmailAndPassword(email, password);
+  } catch (err) {
+    console.error(err);
+    alert(err.message);
+  }
+};
+
+const registerWithEmailAndPassword = async (name, email, password) => {
+  try {
+    const res = await auth.createUserWithEmailAndPassword(email, password);
+    const user = res.user;
+    await db.collection("users").add({
+      uid: user.uid,
+      name,
+      authProvider: "local",
+      email,
+    });
+  } catch (err) {
+    console.error(err);
+    alert(err.message);
+  }
+};
+
+const sendPasswordResetEmail = async (email) => {
+  try {
+    await auth.sendPasswordResetEmail(email);
+    alert("Password reset link sent!");
+  } catch (err) {
+    console.error(err);
+    alert(err.message);
+  }
+};
+
+const logout = () => {
+  auth.signOut();
+};
+
+export {
+  auth,
+  db,
+  signInWithGoogle,
+  signInWithEmailAndPassword,
+  registerWithEmailAndPassword,
+  sendPasswordResetEmail,
+  logout,
+};
